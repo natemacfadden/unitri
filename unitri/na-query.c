6de9fe6fab59dbf7f26ac2b97d88949addac8cb5
nate <natemacfadden@gmail.com>
2026-06-23 18:13:33 -0400

    headers

--- unitri/na-query.c
+++ unitri/na-query.c
@@ -1,3 +1,19 @@
+/* =============================================================================
+ *    Copyright (C) 2026  Nate MacFadden for the Liam McAllister Group
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ============================================================================= */
 // CLI driver for the lattice-triangulation counter.  The counting code lives
 // in na_query.h (STB single-header style); this translation unit pulls in its
 // implementation and forwards to the entry point.  Build-time configuration
